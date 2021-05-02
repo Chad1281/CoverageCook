af88cb4fca12fba29418d286cf30d0d5eaec5ce6
Chad Cook <charliert13@yahoo.com>
2021-05-02 08:49:55 -0500

    added sales cards to landing page

---
+++ src/assets/data/landingCards.js
@@ -0,0 +1,24 @@
+import NotePad from "../images/landingCards/notepad.png";
+import Law from "../images/landingCards/law.png";
+import Loan from "../images/landingCards/loan.png";
+
+const LandingCards = [
+    {
+        src: NotePad,
+        alt: "NotePad",
+        heading: "ANSWER SOME BASIC QUESTIONS",
+        paragraph: "YOU SHOULD HAVE ALL NEEDED INFO IN YOUR HEAD"
+    }, {
+        src: Law,
+        alt: "Law symbol",
+        heading: "COMPARE QUOTES SIDE BY SIDE",
+        paragraph: "YOU WON'T GET HARASSED BY A BUNCH OF AGENTS.  YOU'LL SEE YOUR QUOTES, AND ONLY ONE AGENT WILL CALL TO MAKE SURE YOU GET THE BEST DEAL."
+    }, {
+        src: Loan,
+        alt: "Loan symbol",
+        heading: "SAVE MONEY",
+        paragraph: "SLEEP WELL AT NIGHT KNOWING THAT YOU'RE WITH THE COMPANY THAT WANTS YOU MOST."
+    },
+]
+
+export default LandingCards;
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CarrierImgLinks from "../../assets/data/carrierImgLinks";
+import LandingCards from "../../assets/data/landingCards";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
@@ -12,8 +13,6 @@ const LandingPage = () => {
     return(
         <div id="landingContainer">
             <Header />
-            <Navbar />
-            {/* <div className="spacer" /> */}
             <div id="carrierImgsContainer">
                 <div id="carrierImgs">
                     {CarrierImgLinks.map(function(image) {
@@ -23,9 +22,21 @@ const LandingPage = () => {
                     })}
                 </div>
             </div>
-            <div>
-
+            <Navbar />
+            <div className="spacer" />
+            <div id="landingCardsWrapper">
+                {LandingCards.map(function(card) {
+                    return(
+                        <div className="card">
+                            <img src={card.src} alt={card.alt} />
+                            <h2>{card.heading}</h2>
+                            <p>{card.paragraph}</p>
+                        </div>                        
+                    )
+                })}    
             </div>
+            <div className="spacer" />
+                        
             <Footer />
         </div>
     )
