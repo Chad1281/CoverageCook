6cc4eff2d6026940de6972f2ae7cf6151af1de8d
Chad Cook <charliert13@yahoo.com>
2021-05-02 05:11:16 -0500

    made carrier image animation fixed at bottom, images are mapped

---
+++ src/assets/data/carrierImgLinks.js
@@ -0,0 +1,98 @@
+import Aegis from "../images/carriers/aegis.png";
+import ASI from "../images/carriers/asi.png";
+import BerkshireHathaway from "../images/carriers/berkshireHathaway.png";
+import BristolWest from "../images/carriers/bristolWest.png";
+import Encompass from "../images/carriers/encompass.png";
+import Foremost from "../images/carriers/foremost.png";
+import Guard from "../images/carriers/guard.png";
+import Hagerty from "../images/carriers/hagerty.png";
+import Hartford from "../images/carriers/hartford.png";
+import Kemper from "../images/carriers/kemper.png";
+import LibertyMutual from "../images/carriers/libertyMutual.png";
+import MetLife from "../images/carriers/metLife.png";
+import NationWide from "../images/carriers/nationWide.png";
+import PersonalUmbrella from "../images/carriers/personalUmbrella.gif";
+import Progressive from "../images/carriers/progressive.png";
+import SouthWestern from "../images/carriers/southWestern.png";
+import StateAuto from "../images/carriers/stateAuto.png";
+import TowerHill from "../images/carriers/towerHill.png";
+import Travelers from "../images/carriers/travelers.png";
+import Trexis from "../images/carriers/trexis.png";
+import TWIA from "../images/carriers/twia.png";
+import UniversalNorthAmerica from "../images/carriers/universalNorthAmerica.png";
+import USLI from "../images/carriers/usli.png";
+
+const CarrierImgLinks = [
+    {
+        src: Aegis,
+        alt: "Aegis logo"
+    },  {
+        src: ASI,
+        alt: "ASI logo"
+    }, {
+        src: BerkshireHathaway,
+        alt: "Berkshire Hathaway logo"
+    }, {
+        src: BristolWest,
+        alt: "Bristol West logo"
+    }, {
+        src: Encompass,
+        alt: "Encompass logo"
+    }, {
+        src: Foremost,
+        alt: "Foremost logo"
+    }, {
+        src: Guard,
+        alt: "Guard logo"
+    }, {
+        src: Hagerty,
+        alt: "Hagerty logo"
+    }, {
+        src: Hartford,
+        alt: "Hartford logo"
+    }, {
+        src: Kemper,
+        alt: "Kemper logo"
+    }, {
+        src: LibertyMutual,
+        alt: "Liberty Mutual logo"
+    }, {
+        src: MetLife,
+        alt: "MetLife logo"
+    }, {
+        src: NationWide,
+        alt: "NationWide logo"
+    }, {
+        src: PersonalUmbrella,
+        alt: "Personal Umbrella logo"
+    }, {
+        src: Progressive,
+        alt: "Progressive logo"
+    }, {
+        src: SouthWestern,
+        alt: "South Western logo"
+    }, {
+        src: StateAuto,
+        alt: "State Auto logo"
+    }, {
+        src: TowerHill,
+        alt: "Tower Hill logo"
+    }, {
+        src: Travelers,
+        alt: "Travelers logo"
+    }, {
+        src: Trexis,
+        alt: "Trexis logo"
+    }, {
+        src: TWIA,
+        alt: "TWIA logo"
+    }, {
+        src: UniversalNorthAmerica,
+        alt: "Universal North America logo"
+    }, {
+        src: USLI,
+        alt: "USLI logo"
+    },
+]
+
+export default CarrierImgLinks;
