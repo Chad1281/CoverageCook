d3edcc6eea72a08ac05a20c8021f0996f5979bcf
Chad Cook <charliert13@yahoo.com>
2021-05-06 21:38:07 -0500

    created php file for sending contact form through email

---
+++ src/components/ContactForm/contact.php
@@ -0,0 +1,20 @@
+<?php
+    if(isset($_POST['submit'])){
+        $fname=$_POST['fname'];
+        $email=$_POST['email'];
+        $phone=$_POST['phone'];
+        $comment=$_POST['comment'];
+
+        $to='ccook@fireflyagency.com';
+        $subject='InsuranceCook Form Submission';
+        $message='Name: '.$fname.'\n'.'Phone: '.$phone.'\n'. 'Comment: '.'\n\n'.$comment;
+        $headers='From: '.$email;
+
+        if(mail($to, $subject, $message, $headers)){
+            echo '<h1>Sent Successfully: Thank you'.' '.$name.', We will conact you shortly!</h1>';
+        }
+        else{
+            echo 'Something went wrong! Try resubmitting form.';
+        }
+    }
+?>
--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./style.css";
 import Phone from "../../assets/images/other/phone.png";
+import Contact from "./contact.php";
 
 export default function ContactForm() {
     return(
         <div className="contact hide">
             <h2>Contact Request</h2>
-            <form>
+            <form action={Contact} method="post" name="form">
                 <label for="name">Full Name:</label><br />
                 <input type="text" id="fname" name="fname" placeholder="Your name" /><br />
-                <label for="phonhe">Phone Number:</label><br />
+                <label for="email">Email:</label><br />
+                <input type="text" id="email" name="email" placeholder="Your email" /><br />
+                <label for="phone">Phone Number:</label><br />
                 <input type="tel" id="phone" name="phone" placeholder="555-555-5555" /><br />
                 <label for="comment">Comments:</label><br />
                 <textarea id="comment" name="comment" placeholder="Leave a comment"></textarea><br />
-                <input type="submit" value="Submit" />
+                <input type="submit" name="submit" value="Submit" />
             </form>
             <div id="agent">
                 <p><a href="tel:512-644-3039">Call Chad Cook 512-312-7360</a></p>
