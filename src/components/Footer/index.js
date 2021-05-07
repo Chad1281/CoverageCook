4269d44672f2f39982e5068238d644073b66d390
Chad Cook <charliert13@yahoo.com>
2021-05-07 11:06:52 -0500

    changed alt on footer to title

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,9 +23,9 @@ export default function Footer() {
             <div id="footerContainer">
                 <img src={ FireflyBW } alt="Firefly Logo" />
                 <h3>Cook Firefly Insurance Agency</h3>
-                <p><a href="tel:512-644-3039" alt="Phone Number">Contact Chad Cook 512-312-7360</a></p>
-                <p><a href="tel:833-436-0202" alt="Toll Free Number">Toll Free 1(833) 436-0202</a></p>
-                <address><a href="mailto:ccook@fireflyagency.com" title="Email" alt="email">ccook@fireflyagency.com</a></address>
+                <p><a href="tel:512-644-3039" title="Phone Number">Contact Chad Cook 512-312-7360</a></p>
+                <p><a href="tel:833-436-0202" title="Toll Free Number">Toll Free 1(833) 436-0202</a></p>
+                <address><a href="mailto:ccook@fireflyagency.com" title="Email">ccook@fireflyagency.com</a></address>
             </div>
         </div>
     )
