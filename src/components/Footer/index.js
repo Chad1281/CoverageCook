ef56562081c2b1c53155581223594b3dcb0a8083
Chad Cook <charliert13@yahoo.com>
2021-05-07 10:50:49 -0500

    corrected toll free number

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -24,7 +24,7 @@ export default function Footer() {
                 <img src={ FireflyBW } alt="Firefly Logo" />
                 <h3>Cook Firefly Insurance Agency</h3>
                 <p><a href="tel:512-644-3039" alt="Phone Number">Contact Chad Cook 512-312-7360</a></p>
-                <p><a href="tel:833-360-0202" alt="Toll Free Number">Toll Free 1(833) 436-0202</a></p>
+                <p><a href="tel:833-436-0202" alt="Toll Free Number">Toll Free 1(833) 436-0202</a></p>
                 <address><a href="mailto:ccook@fireflyagency.com" title="Email" alt="email">ccook@fireflyagency.com</a></address>
             </div>
         </div>
