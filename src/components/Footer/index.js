3232d70ecf807fc92de67916d8e1bc0b64a19353
Chad Cook <charliert13@yahoo.com>
2021-10-11 03:15:24 -0500

    gave images explicit widths and heights

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,20 +6,6 @@ import "./style.css";
 export default function Footer() {
     return(
         <div>
-            {/* <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/carriers">Carriers</Link>
-                </li>
-                <li>
-                    <Link to="/claims">Claims</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul> */}
             <div id="footerContainer">
                 <img src={ FireflyBW } alt="Firefly Logo" />
                 <h3>Cook Firefly Insurance Agency</h3>
